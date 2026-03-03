0154f85eed506c8b66a3b7735f24ae2fb3ec8368
K9 <pfalgg7@gmail.com>
2026-03-03 20:54:30 -0300

    ponto7

--- script.js
+++ script.js
@@ -10,7 +10,15 @@ let soma = 0;
 for (let i = 1; i <= 10; i++) {
     soma = soma + i;
 }
-console.log(soma);
-alert(soma);
+/*alert("A soma dos números de 1 a 10 é: " + soma);*/
+
+/*programa que conta apenas os numeros pares de 1 a 20*/
+let pares = 0;
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        pares = pares + i;
+    }
+}
+/*alert("soma dos numeros pares de 1 ate 20:" + pares);*/
 
 //#endregion
