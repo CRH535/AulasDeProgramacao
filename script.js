5f6774288cf725572c65821e31bcfcc80303e270
K9 <pfalgg7@gmail.com>
2026-03-03 22:00:35 -0300

    ponto10

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
-function submitName(name) {
-    alert("Hello, " + name + "!");
-}
-
 //#region
 
 /* aulas do petronio */
 
 let soma = 0;
 for (let i = 1; i <= 10; i++) {
-    soma = soma + i;
+  soma = soma + i;
 }
 /*alert("A soma dos números de 1 a 10 é: " + soma);*/
 
 /*programa que conta apenas os numeros pares de 1 a 20*/
 let pares = 0;
 for (let i = 1; i <= 20; i++) {
-    if (i % 2 === 0) {
-        pares = pares + i;
-    }
+  if (i % 2 === 0) {
+    pares = pares + i;
+  }
 }
 /*alert("soma dos numeros pares de 1 ate 20:" + pares);*/
 
+//#endregion
+
+//#region
+//aula do wilgner
+function submitLogin() {
+  const username = document.getElementById("nameInput").value;
+  const password = document.getElementById("senhaInput").value;
+  if (username === "admin" && password === "1234") {
+    alert("Login bem-sucedido!");
+    window.location.href = "pagina2.html";
+  } else {
+    alert("Credenciais inválidas. Tente novamente.");
+  }
+}
 //#endregion
