eae4b838842061245a55b5efe10e5e163c22798a
K9 <pfalgg7@gmail.com>
2026-03-09 21:26:32 -0300

    adicao petronio

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
 //#region
-
-/* aulas do petronio */
-
-let soma = 0;
-for (let i = 1; i <= 10; i++) {
-  soma = soma + i;
-}
-/*alert("A soma dos números de 1 a 10 é: " + soma);*/
-
-/*programa que conta apenas os numeros pares de 1 a 20*/
-let pares = 0;
-for (let i = 1; i <= 20; i++) {
-  if (i % 2 === 0) {
-    pares = pares + i;
-  }
-}
-/*alert("soma dos numeros pares de 1 ate 20:" + pares);*/
-
-//#endregion
-
-//#region
 //aula do wilgner
 
 // Função para validar o login
